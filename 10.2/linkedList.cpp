b82ecae45371b47cd36fff37b6ff1c7b7afe8f15
SenthilShelke <senthil.shelke@gmail.com>
2025-05-29 19:33:04 -0400

    added linked list

---
+++ 10.2/linkedList.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+private:
+    int value;
+    Node* next;
+    Node* prev;
+
+    friend class LinkedList; 
+
+public:
+    Node(int val, Node* nextNode = nullptr, Node* prevNode = nullptr) : value(val), next(nextNode), prev(prevNode) {}
+};
+
+class LinkedList {
+private:
+    Node* head;
+    Node* tail;
+    int size;
+
+public:
+    LinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    ~LinkedList() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* nextNode = current->next;
+            delete current;
+            current = nextNode;
+        }
+    }
+
+    void prepend(int x) {
+        Node* node = new Node(x, head, nullptr);
+
+        if(head != nullptr) {
+            head->prev = node;
+        } else {
+            tail = node;
+        }
+        head = node;
+        size++;
+    }
+
+    void append(int x) {
+        Node* node = new Node(x, nullptr, tail);
+
+        if(tail != nullptr) {
+            tail->next = node;
+        } else {
+            head = node;
+        }
+        tail = node;
+        size++;
+    }
+
+    void removeFirst() {
+        if(head == nullptr) {
+            cout << "List is empty" << endl;
+            return;
+        }
+        Node* temp = head;
+        head = head->next;
+        if(head != nullptr) {
+            head->prev = nullptr;
+        } else {
+            tail = nullptr; 
+        }
+        delete temp;
+        size--;
+    }
+
+    void removeLast() {
+        if(tail == nullptr) {
+            cout << "List is empty" << endl;
+            return;
+        }
+        Node* temp = tail;
+        tail = tail->prev;
+        if(tail != nullptr) {
+            tail->next = nullptr;
+        } else {
+            head = nullptr; 
+        }
+        delete temp;
+        size--;
+    }
+    
+};
